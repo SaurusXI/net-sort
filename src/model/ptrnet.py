3e249209f1da0fd6b4b9730b12d95f78c752f193
SaurusXI <vermashantanu@hotmail.com>
2020-12-17 12:15:14 +0530

    fix(decoder): use reduction weights instead of bias

--- src/model/ptrnet.py
+++ src/model/ptrnet.py
@@ -12,29 +12,35 @@ class PtrNet:
         self.encoder = Encoder(1)
         self.decoder = Decoder(1)
         self.weights = {
-            'encoder': np.random.random([1, CONTEXT_LEN]),
-            'decoder': np.random.random([1, CONTEXT_LEN])
+            'encoder': np.random.random([CONTEXT_LEN, CONTEXT_LEN]),
+            'decoder': np.random.random([CONTEXT_LEN, CONTEXT_LEN]),
+            'reduction': np.random.random([1, CONTEXT_LEN])
         }
-        self.bias = np.random.random([1, 1])
 
     def forward(self, x):
         encoded_activation, encoded_context = self.encoder.forward(x)
         timesteps = x.shape[0]
         self.decoder.forward(encoded_activation, encoded_context, timesteps)
 
-        encoder_states = self.encoder.get_activations()
-        decoder_states = self.decoder.get_activations()
+        self.encoder_states = self.encoder.get_activations()
+        self.decoder_states = self.decoder.get_activations()
 
+        self.x = x
         self.y = np.array([])
 
         for i in range(timesteps):
-            di = decoder_states[i]
+            di = self.decoder_states[i]
             ui = np.array([])
             for j in range(timesteps):
-                ej = encoder_states[j]
-                z = (self.weights['encoder'] @ ej) + (self.weights['decoder'] @ di) + self.bias
-                uij = np.tanh(z)
+                ej = self.encoder_states[j]
+                z = (self.weights['encoder'] @ ej) + \
+                    (self.weights['decoder'] @ di) + self.bias
+                uij = self.weights['reduction'] @ np.tanh(z)
                 ui.append(uij[0])
 
             self.y.append(softmax(ui))
 
+    def output(self):
+        indices = np.argmax(self.y, axis=1)
+        out = [self.x[i] for i in indices]
+        return out
