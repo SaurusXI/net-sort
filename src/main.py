efa303feee8636fffe90896d6eeecf83b1048d35
SaurusXI <vermashantanu@hotmail.com>
2020-12-21 02:02:53 +0530

    checkpoint: forward and backprop implemented

--- src/main.py
+++ src/main.py
@@ -3,5 +3,8 @@ import numpy as np
 
 
 sorter = PtrNet()
-x = np.array([1, 4, 2, 5, 6])
-print(sorter.forward(x))
+x = np.array([[1, 4, 2, 5, 6]])
+y = np.array([[0, 1, 2, 3, 4]])
+sorter.train(x, y, 100)
+sorter.forward(np.array([1, 4, 2, 5, 6]))
+print(sorter.output())
