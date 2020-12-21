fix: corrections in backprop equations

src/LSTM/cell.py
@@ -63,23 +63,26 @@ class Cell:
         out_len = activation.shape[0]
 
         # Gradients for LSTM cell gates
-        # dcontext = dactivation * out_gate * (1 - np.square(np.tanh(context)))
+        dcontext += dactivation * out_gate * (1 - np.square(np.tanh(context)))
         dout_gate = dactivation * np.tanh(context) * out_gate * (1 - out_gate)
+        dforget_gate = (dcontext * context_prev) * forget_gate * (1 - forget_gate)
+        dupdate_gate = (dcontext * candidate) * update_gate * (1 - update_gate)
+        dcandidate = (dcontext * update_gate) * (1 - np.square(np.tanh(context)))
+        
+        # dupdate_gate = (dcontext * candidate +
+        #                 out_gate * (1 - np.square(np.tanh(context))) *
+        #                 candidate * dactivation) * update_gate * \
+        #     (1 - update_gate)
 
-        dupdate_gate = (dcontext * candidate +
-                        out_gate * (1 - np.square(np.tanh(context))) *
-                        candidate * dactivation) * update_gate * \
-            (1 - update_gate)
+        # dforget_gate = (dcontext * context_prev +
+        #                 out_gate * (1 - np.square(np.tanh(context))) *
+        #                 context_prev * dactivation) * forget_gate * \
+        #     (1 - forget_gate)
 
-        dforget_gate = (dcontext * context_prev +
-                        out_gate * (1 - np.square(np.tanh(context))) *
-                        context_prev * dactivation) * forget_gate * \
-            (1 - forget_gate)
-
-        dcandidate = (dcontext * update_gate +
-                      out_gate * (1 - np.square(np.tanh(context))) *
-                      update_gate * dactivation) * \
-            (1 - np.square(candidate))
+        # dcandidate = (dcontext * update_gate +
+        #               out_gate * (1 - np.square(np.tanh(context))) *
+        #               update_gate * dactivation) * \
+        #     (1 - np.square(candidate))
 
         if take_input:
             # print(f'xShape {x}')

src/decoder/decoder.py
@@ -65,20 +65,23 @@ class Decoder:
 
         return self.activations
 
-    def backprop(self, dcontexts):
-        dactivation = np.zeros([CONTEXT_LEN, 1])
+    def backprop(self, dactivations):
+        dcontext = np.zeros([CONTEXT_LEN, 1])
+        dactivation_future = dcontext
 
         for t in reversed(range(self.timesteps)):
+            dactivation = dactivation_future + dactivations[t, :, :]
             grad = self.cell.backprop(
                 dactivation,
-                dcontexts[t, :, :],
+                dcontext,
                 self.caches[t],
                 False
             )
-            dactivation = grad['activ_prev']
+            dactivation_future = grad['activ_prev']
+            dcontext = grad['context_prev']
             self.update_grads(grad)
 
-        return dactivation
+        return dactivation_future, dcontext
 
     def update_grads(self, grad):
         self.gradients['weights_forget'] += grad['weights_forget']

src/main.py
@@ -1,10 +1,15 @@
-from model.ptrnet import PtrNet
+from model.seq2seq import Seq2Seq
 import numpy as np
 
 
-sorter = PtrNet()
+sorter = Seq2Seq()
 x = np.array([[1, 4, 2, 5, 6]])
 y = np.array([[0, 1, 2, 3, 4]])
-sorter.train(x, y, 100)
 sorter.forward(np.array([1, 4, 2, 5, 6]))
+print(sorter.out)
+# sorter.compute_loss(y[0])
+# sorter.backprop()
+# sorter.apply_gradients()
+sorter.train(x, y, 100)
+print(sorter.out)
 print(sorter.output())