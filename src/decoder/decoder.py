59262e765f6f0ff9a6a36610d88ab35be13b07c3
SaurusXI <vermashantanu@hotmail.com>
2021-01-30 17:58:25 +0530

    feat(decoder): add ptrnet backprop and adam optimizer

--- src/decoder/decoder.py
+++ src/decoder/decoder.py
@@ -4,7 +4,7 @@ from model.utils import relu, OHE, drelu
 from scipy.special import softmax
 
 
-CONTEXT_LEN = 64
+CONTEXT_LEN = 16
 
 
 class Decoder:
@@ -16,16 +16,17 @@ class Decoder:
         # Initialize weights and biases
         weights_shape = [CONTEXT_LEN, CONTEXT_LEN]
         bias_shape = [CONTEXT_LEN, 1]
+        limit = 4 * ((3 / CONTEXT_LEN) ** 0.5)
+        out_limit = CONTEXT_LEN ** 0.5
         self.weights = {
-            'update': np.random.random(weights_shape) / 1e4,
-            'forget': np.random.random(weights_shape) / 1e4,
-            'candidate': np.random.random(weights_shape) / 1e4,
-            'output': np.random.random(weights_shape) / 1e4,
-            'W1': np.random.random(weights_shape + self.output_len) / 1e4,
-            'W2': np.random.random(weights_shape + self.output_len) / 1e4,
-            'v': np.random.random([CONTEXT_LEN + self.output_len, 1]) / 1e4
+            'update': np.random.default_rng().uniform(-(limit), limit, weights_shape),
+            'forget': np.random.default_rng().uniform(-(limit), limit, weights_shape),
+            'candidate': np.random.default_rng().uniform(-(limit) / 4, limit / 4, weights_shape),
+            'output': np.random.default_rng().uniform(-(limit), limit, weights_shape),
+            'W1': np.random.default_rng().uniform(-(limit) / 4, limit / 4, weights_shape),
+            'W2': np.random.default_rng().uniform(-(limit) / 4, limit / 4, weights_shape),
+            'v': np.random.default_rng().uniform(-out_limit, out_limit, [1, CONTEXT_LEN])
         }
-        # print(self.weights['y'])
         self.biases = {
             'update': np.random.random([CONTEXT_LEN, 1]),
             'forget': np.ones([CONTEXT_LEN, 1]),
@@ -46,13 +47,45 @@ class Decoder:
             'weights_update': np.zeros(weights_shape),
             'weights_output': np.zeros(weights_shape),
             'weights_candidate': np.zeros(weights_shape),
-            'weights_y': np.zeros([self.output_len, CONTEXT_LEN]),
+            'weights_W1': np.zeros(weights_shape),
+            'weights_W2': np.zeros(weights_shape),
+            'weights_v': np.zeros([1, CONTEXT_LEN]),
             'bias_forget': np.zeros(bias_shape),
             'bias_update': np.zeros(bias_shape),
             'bias_output': np.zeros(bias_shape),
             'bias_candidate': np.zeros(bias_shape),
             'bias_y': np.zeros([self.output_len, 1]),
-            'output_activation': np.zeros(bias_shape),
+            'encoder_activations': []
+        }
+
+        self.accumulated_velocity = {
+            'weights_forget': np.zeros(weights_shape),
+            'weights_update': np.zeros(weights_shape),
+            'weights_output': np.zeros(weights_shape),
+            'weights_candidate': np.zeros(weights_shape),
+            'weights_W1': np.zeros(weights_shape),
+            'weights_W2': np.zeros(weights_shape),
+            'weights_v': np.zeros([1, CONTEXT_LEN]),
+            'bias_forget': np.zeros(bias_shape),
+            'bias_update': np.zeros(bias_shape),
+            'bias_output': np.zeros(bias_shape),
+            'bias_candidate': np.zeros(bias_shape),
+            'bias_y': np.zeros([self.output_len, 1])
+        }
+
+        self.accumulated_S = {
+            'weights_forget': np.zeros(weights_shape),
+            'weights_update': np.zeros(weights_shape),
+            'weights_output': np.zeros(weights_shape),
+            'weights_candidate': np.zeros(weights_shape),
+            'weights_W1': np.zeros(weights_shape),
+            'weights_W2': np.zeros(weights_shape),
+            'weights_v': np.zeros([1, CONTEXT_LEN]),
+            'bias_forget': np.zeros(bias_shape),
+            'bias_update': np.zeros(bias_shape),
+            'bias_output': np.zeros(bias_shape),
+            'bias_candidate': np.zeros(bias_shape),
+            'bias_y': np.zeros([self.output_len, 1])
         }
 
     def forward(self, encoded_activations, encoded_contexts, timesteps, debug=False):
@@ -65,24 +98,22 @@ class Decoder:
         activation = encoded_activations[-1]
 
         for t in range(timesteps):
-            # if t == 3:
-            #     1/0
             activation, context, cache = self.cell.forward(
                 None, activation, context, self.weights, self.biases, False
             )
             ui = []
+            # print(encoded_activations.shape)
             for enc_act in encoded_activations:
-                uij = self.weights['v'].T @ np.tanh(
+                uij = self.weights['v'] @ np.tanh(
                     self.weights['W1'] @ enc_act + 
                     self.weights['W2'] @ activation
                 )
-                ui.append(uij)
+                ui.append(uij.item())
 
             prediction = softmax(
                 np.array(ui) 
                 / self.temperature
             )
-            # print(prediction[:60])
             self.predictions.append(prediction)
             self.activations.append(activation)
             self.contexts.append(context)
@@ -94,37 +125,60 @@ class Decoder:
         self.input_activation = encoded_activations
         self.input_context = encoded_contexts
 
-        if debug:
-            with open('activations.log', 'w') as f:
-                print(np.array(self.predictions), file=f)
         return self.predictions
 
-    def backprop(self, ground_truth):
+    def backprop(self, ground_truth, encoded_activations):
         dcontext = np.zeros([CONTEXT_LEN, 1])
         dactiv_prev = np.zeros([CONTEXT_LEN, 1])
 
-        for t in reversed(range(self.timesteps)):
-            do = self.predictions[t] - OHE(ground_truth[t], self.output_len).reshape(-1, 1)
-            self.gradients['output_activation'] = dactiv_prev + ((self.weights['y'].T @ do) / self.temperature)
+        dv = np.zeros([1, CONTEXT_LEN])
+        dW1 = np.zeros([CONTEXT_LEN, 1])
+        dW2 = np.zeros([CONTEXT_LEN, 1])
+
+        self.gradients['encoder_activations'] = [np.zeros([CONTEXT_LEN, 1]) for i in range(self.timesteps)]
+
+        for i in reversed(range(self.timesteps)):
+            expected = OHE(ground_truth[i], self.timesteps).reshape(-1, 1)
+            dui = (self.predictions[i].reshape(-1, 1) - expected) / self.temperature
+            ddi = np.zeros([CONTEXT_LEN, 1])
+            dvj = np.zeros([1, CONTEXT_LEN])
+            dW1j = np.zeros([CONTEXT_LEN, 1])
+            dW2i = np.zeros([CONTEXT_LEN, 1])
+
+            for j, duij in enumerate(dui):
+                inner = np.tanh(self.weights['W1'] @ encoded_activations[j] +
+                    self.weights['W2'] @ self.activations[i])
+                dvj += duij.item() * inner.T
+                dW1j += duij.item() * (1 - np.square(inner)) * encoded_activations[j]
+                dW2i += duij.item() * (1 - np.square(inner))
+                ddi += duij.item() * (1 - np.square(inner))
+                self.gradients['encoder_activations'][j] += duij.item() * (1 - np.square(inner))
+
+            dW1 += dW1j
+            dW2 += dW2i * self.activations[i]
+            dv += dvj
+            ddi = ((self.weights['W2'] @ self.weights['v'].T) * ddi) + dactiv_prev
 
             grad = self.cell.backprop(
-                self.gradients['output_activation'],
+                ddi,
                 dcontext,
-                self.caches[t],
+                self.caches[i],
                 False
             )
             dcontext = grad['context_prev']
             dactiv_prev = grad['activ_prev']
-
-            grad['weights_y'] = (do @ self.activations[t].T) / self.temperature
-            grad['bias_y'] = do / self.temperature
             self.update_grads(grad)
+        
+        self.gradients['weights_W1'] = dW1 @ self.weights['v']
+        self.gradients['weights_W2'] = dW2 @ self.weights['v']
+        self.gradients['weights_v'] = dv
+        self.gradients['encoder_activations'] = list(
+            map(lambda x: x * (self.weights['W1'] @ self.weights['v'].T), self.gradients['encoder_activations'])
+        )
 
-        return dactiv_prev, dcontext
+        return dactiv_prev, dcontext, self.gradients['encoder_activations']
 
     def update_grads(self, grad, clipping=True):
-        # print(self.gradients['weights_forget'].shape)
-        # print(grad['weights_forget'].shape)
         self.gradients['weights_forget'] += grad['weights_forget']
         self.gradients['weights_update'] += grad['weights_update']
         self.gradients['weights_output'] += grad['weights_output']
@@ -133,8 +187,6 @@ class Decoder:
         self.gradients['bias_update'] += grad['bias_update']
         self.gradients['bias_output'] += grad['bias_output']
         self.gradients['bias_candidate'] += grad['bias_candidate']
-        self.gradients['weights_y'] += grad['weights_y']
-        self.gradients['bias_y'] += grad['bias_y']
         if clipping:
             self.clip_grads()
 
@@ -146,14 +198,52 @@ class Decoder:
             'weights_update': np.zeros(weights_shape),
             'weights_output': np.zeros(weights_shape),
             'weights_candidate': np.zeros(weights_shape),
-            'weights_y': np.zeros([self.output_len, CONTEXT_LEN]),
+            'weights_W1': np.zeros(weights_shape),
+            'weights_W2': np.zeros(weights_shape),
+            'weights_v': np.zeros([1, CONTEXT_LEN]),
             'bias_forget': np.zeros(bias_shape),
             'bias_update': np.zeros(bias_shape),
             'bias_output': np.zeros(bias_shape),
             'bias_candidate': np.zeros(bias_shape),
             'bias_y': np.zeros([self.output_len, 1]),
-            'output_activation': np.zeros(bias_shape),
+            'encoder_activations': []
         }
+
+    def reset_accum(self):
+
+        weights_shape = [CONTEXT_LEN, CONTEXT_LEN]
+        bias_shape = [CONTEXT_LEN, 1]
+
+        self.accumulated_velocity = {
+            'weights_forget': np.zeros(weights_shape),
+            'weights_update': np.zeros(weights_shape),
+            'weights_output': np.zeros(weights_shape),
+            'weights_candidate': np.zeros(weights_shape),
+            'weights_W1': np.zeros(weights_shape),
+            'weights_W2': np.zeros(weights_shape),
+            'weights_v': np.zeros([1, CONTEXT_LEN]),
+            'bias_forget': np.zeros(bias_shape),
+            'bias_update': np.zeros(bias_shape),
+            'bias_output': np.zeros(bias_shape),
+            'bias_candidate': np.zeros(bias_shape),
+            'bias_y': np.zeros([self.output_len, 1])
+        }
+
+        self.accumulated_S = {
+            'weights_forget': np.zeros(weights_shape),
+            'weights_update': np.zeros(weights_shape),
+            'weights_output': np.zeros(weights_shape),
+            'weights_candidate': np.zeros(weights_shape),
+            'weights_W1': np.zeros(weights_shape),
+            'weights_W2': np.zeros(weights_shape),
+            'weights_v': np.zeros([1, CONTEXT_LEN]),
+            'bias_forget': np.zeros(bias_shape),
+            'bias_update': np.zeros(bias_shape),
+            'bias_output': np.zeros(bias_shape),
+            'bias_candidate': np.zeros(bias_shape),
+            'bias_y': np.zeros([self.output_len, 1])
+        }
+
 
     def get_activations(self):
         return self.contexts
@@ -167,14 +257,33 @@ class Decoder:
         except Exception:
             pass
 
-    def apply_gradients(self, learning_rate=1e-3, momentum = 0.9):
-        self.weights['forget'] -= learning_rate * self.gradients['weights_forget']
-        self.weights['update'] -= learning_rate * self.gradients['weights_update']
-        self.weights['output'] -= learning_rate * self.gradients['weights_output']
-        self.weights['candidate'] -= learning_rate * self.gradients['weights_candidate']
-        self.weights['y'] -= learning_rate * self.gradients['weights_y']
-        self.biases['forget'] -= learning_rate * self.gradients['bias_forget']
-        self.biases['update'] -= learning_rate * self.gradients['bias_update']
-        self.biases['output'] -= learning_rate * self.gradients['bias_output']
-        self.biases['candidate'] -= learning_rate * self.gradients['bias_candidate']
-        self.biases['y'] -= learning_rate * self.gradients['bias_y']
+    def apply_gradients(self, timestep, learning_rate=1e-3, momentum=0.9, beta = 0.999, epsilon = 1e-8):
+        for k, v in self.weights.items():
+            grad_key = 'weights_' + k
+            self.accumulated_velocity[grad_key] = momentum * self.accumulated_velocity[grad_key] + \
+                (1 - momentum) * self.gradients[grad_key]
+            self.accumulated_S[grad_key] = beta * self.accumulated_S[grad_key] + \
+                (1 - beta) * np.square(self.gradients[grad_key])
+
+            # Correction terms
+            v_corrected = self.accumulated_velocity[grad_key] / (1 - (momentum ** timestep))
+            s_corrected = self.accumulated_S[grad_key] / (1 - (beta ** timestep))
+
+            self.weights[k] -= learning_rate * v_corrected / np.sqrt(
+                s_corrected + epsilon
+            )
+
+        for k, v in self.biases.items():
+            grad_key = 'bias_' + k
+            self.accumulated_velocity[grad_key] = momentum * self.accumulated_velocity[grad_key] + \
+                (1 - momentum) * self.gradients[grad_key]
+            self.accumulated_S[grad_key] = beta * self.accumulated_S[grad_key] + \
+                (1 - beta) * np.square(self.gradients[grad_key])
+
+            # Correction terms
+            v_corrected = self.accumulated_velocity[grad_key] / (1 - (momentum ** timestep))
+            s_corrected = self.accumulated_S[grad_key] / (1 - (beta ** timestep))
+
+            self.biases[k] -= learning_rate * v_corrected / np.sqrt(
+                s_corrected + epsilon
+            )
