537fd522bba4ef4459175c4a9492d060b6ad5503
SaurusXI <vermashantanu@hotmail.com>
2021-01-30 17:59:44 +0530

    feat(model): add adam optimization and activations logging

--- src/model/seq2seq.py
+++ src/model/seq2seq.py
@@ -3,68 +3,78 @@ from decoder.decoder import Decoder
 from model.utils import categorical_cross_entropy, OHE
 import numpy as np
 
-CONTEXT_LEN = 64
-MAX_NUM = 10
+CONTEXT_LEN = 16
+MAX_NUM = 20
 
 
 class Seq2Seq:
     def __init__(self):
         self.encoder = Encoder(MAX_NUM)
-        self.decoder = Decoder(MAX_NUM, 0.5)
+        self.decoder = Decoder(MAX_NUM, 1)
 
         # Forward prop stuff
         self.input = None
+        self.encoded_activations = None
+        self.encoded_contexts = None
         self.out = []
 
         # Compute loss stuff
         self.Loss = None
 
     def forward(self, x, debug=False):
-        encoded_activations, encoded_contexts = self.encoder.forward(x)
+        self.encoded_activations, self.encoded_contexts = self.encoder.forward(x)
         self.timesteps = x.shape[0]
         self.out = self.decoder.forward(
-            encoded_activations, encoded_contexts, self.timesteps, debug
+            self.encoded_activations, self.encoded_contexts, self.timesteps, debug
         )
         self.input = x
 
     def compute_loss(self, ground_truth):
-        self.Loss = categorical_cross_entropy(self.out, OHE(ground_truth, MAX_NUM))
+        N = np.array(ground_truth).shape[0]
+        self.Loss = categorical_cross_entropy(self.out, OHE(ground_truth, N))
         self.ground_truth = ground_truth
         return self.Loss
 
     def backprop(self, ground_truth):
-        dactivation_enc, dcontext_enc = self.decoder.backprop(ground_truth)
-        self.encoder.backprop(dactivation_enc, dcontext_enc)
+        dactivation_enc, dcontext_enc, enc_act_grads = self.decoder.backprop(ground_truth, self.encoded_activations)
+        self.encoder.backprop(dactivation_enc, dcontext_enc, enc_act_grads)
 
-    def apply_gradients(self, learning_rate=1e-4):
-        self.encoder.apply_gradients(learning_rate)
-        self.decoder.apply_gradients(learning_rate)
+    def apply_gradients(self, timestep, learning_rate=2e-2):
+        self.encoder.apply_gradients(timestep, learning_rate)
+        self.decoder.apply_gradients(timestep, learning_rate)
 
     def reset_gradients(self):
         self.encoder.reset_gradients()
         self.decoder.reset_gradients()
 
+    def reset_accum(self):
+        self.encoder.reset_accum()
+        self.decoder.reset_accum()
+
     def train(self, X, y, n_epochs, batch_size=1):
-        # self.forward(np.array([1, 6, 2, 4, 3]))
+        count = 1
         for k in range(n_epochs):
             loss = 0
-            # print(f'Training epoch {k + 1} [', end='')
             for i, x in enumerate(X):
                 self.forward(x)
                 loss += self.compute_loss(y[i])
                 self.backprop(y[i])
                 if i % batch_size == 0:
-                    self.apply_gradients()
+                    self.apply_gradients(count)
+                    count += 1
                     self.reset_gradients()
-                # if i % 100 == 0:
-                    # print('-', end='')
-                # print(f'Loss at sample {i+1} - {loss}')
-            self.apply_gradients()
+                self.debug()
+            self.apply_gradients(count)
             self.reset_gradients()
-            # print(']')
             print(f'Loss for epoch {k+1} - {loss}')
             self.forward(np.array([1, 6, 2, 4, 3]), True)
             print(f'Test sequence {[1, 6, 2, 4, 3]}\nPrediction {self.output()}')
 
     def output(self):
-        return [1 + np.argmax(i) for i in self.out]
+        return [self.input[np.argmax(i)] for i in self.out]
+
+    def debug(self):
+        with open('activations.log', 'w') as f:
+            print(f'Encoder activations:\n{np.array(self.encoder.activations)}', file=f)
+            print(f'Decoder activations:\n{np.array(self.decoder.activations)}', file=f)
+            print(f'Predictions: {np.array(self.decoder.predictions)}', file=f)
